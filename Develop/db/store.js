2e76d5223b333dd838c71630ce7fb8cad875ffcf
Shannon Trainor <sltrainor@outlook.com>
2020-06-10 22:05:12 -0400

    initial commit

---
+++ Develop/db/store.js
@@ -0,0 +1,45 @@
+//create file in db folder store.js
+//store class object
+    //package to install unique ids  -- npm 
+    //inside store.js
+
+//***5 functions in store class object/file */
+//function to read note
+//function to write notes
+//function to get notes
+//functon to add note
+//function to remove notes
+
+//need html & api routes
+// get/add/remove notes - 3 API routes
+    // get/post/delete
+    //routes folder
+        //api route files
+        //html routes
+        //server.js
+//2 routes/notes for HTML
+// * for default route (will load index.html)
+
+
+//import express& api routes
+//create port
+//body parsing w/ static data
+    //from activities
+
+//initialize routes
+    // /api routes
+    // app.use
+
+//app.listen
+
+//START ON STORE.JS
+
+
+//requuire FS in store.js
+
+//doing reading and writing in here
+        //writing includes deleting
+
+        //need util npm package
+
+//module.exports on api & html route pages
