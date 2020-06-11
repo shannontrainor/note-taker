92658b7fff3f256fd0d97f40342b4fe3d3b0709a
Shannon Trainor <sltrainor@outlook.com>
2020-06-11 12:42:58 -0400

    added get route

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,4 +1,10 @@
 const router = require("express").Router();
+const store = require("../db/store")
+const noteData = [];
 
+// read the `db.json` file and return all saved notes as JSON.
+router.get("/api/notes", (req,res) => {
+    store.then(res.json(noteData));
+});
 
 module.exports = router
