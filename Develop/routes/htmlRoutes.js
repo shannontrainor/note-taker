21bc5e60433edaef2d975a1172ef0e13a7c5956e
Shannon Trainor <sltrainor@outlook.com>
2020-06-11 12:42:41 -0400

    added 2 get routes

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,11 +1,22 @@
 const router = require("express").Router();
 
-
-
-
-//2 routes/notes for HTML
-
+//**2 Routes for HTML */
 // * for default route (will load index.html)
+
+//return`index.html` file
+router.get("*", (req,res) => {
+    const file = path.join(__dirname, "public", "assets", "index.html");
+    res.sendFile(file);
+});
+
+
+//return the `notes.html` file.
+router.get("/notes", (req,res) => {
+    const file = path.join(__dirname, "public", "assets", "notes.html");
+    res.sendFile(file);
+});
+
+
 
 
 module.exports = router
