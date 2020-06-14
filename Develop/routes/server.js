d9ad57816eee791f5bb1094c1681da1d82838b4d
Shannon Trainor <sltrainor@outlook.com>
2020-06-14 17:18:07 -0400

    added app.use

--- Develop/routes/server.js
+++ Develop/routes/server.js
@@ -3,5 +3,13 @@ const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
+
+//taken from previous activities
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
