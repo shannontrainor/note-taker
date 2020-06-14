b696d9fbdb3c7500dfb34cf2cf35bf8fa7d68f8a
Shannon Trainor <sltrainor@outlook.com>
2020-06-14 17:39:23 -0400

    added get/add/delete functions to router

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,19 +1,19 @@
 const router = require("express").Router();
 const store = require("../db/store")
-const noteData = [];
+// const noteData = [];
 
 // read the `db.json` file and return all saved notes as JSON.
 router.get("/api/notes", (req,res) => {
-    store.then(res.json(noteData));
+    store.getNotes().then(res.json(notes));
 });
 //rec new note, add to db, return new note 
 router.post("/api/notes", (req, res) => {
-    store.then(res.json(noteData));
+    store.addNotes().then(res.json(notes));
 });
 
 //find note by id, delete note, rewrite to db
 router.delete("/api/notes:id", (req, res) => {
-    store.then(() => res.json({ok: true}))
+    store.deleteNotes().then(() => res.json({ok: true}))
 });
 
 module.exports = router
