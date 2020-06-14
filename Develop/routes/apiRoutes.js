2a0d4b5b60b68bbede0c65a2178450004c76d987
Shannon Trainor <sltrainor@outlook.com>
2020-06-14 16:39:05 -0400

    add get/post/delete api routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -6,5 +6,14 @@ const noteData = [];
 router.get("/api/notes", (req,res) => {
     store.then(res.json(noteData));
 });
+//rec new note, add to db, return new note 
+router.post("/api/notes", (req, res) => {
+    store.then(res.json(noteData));
+});
+
+//find note by id, delete note, rewrite to db
+router.delete("/api/notes:id", (req, res) => {
+    store.then(() => res.json({ok: true}))
+});
 
 module.exports = router
